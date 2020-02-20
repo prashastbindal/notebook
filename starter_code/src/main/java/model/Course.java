749d0bc55238e525e99ace9640199d3d0cda0941
ansharma998 <anirudhSharma@github.com>
2020-02-20 13:13:56 -0500

    new course and note classes

--- starter_code/src/main/java/model/Course.java
+++ starter_code/src/main/java/model/Course.java
@@ -5,11 +5,9 @@ import java.util.Objects;
 public class Course {
   private int id;
   private String name;
-  private String url;
 
-  public Course(String name, String url) {
+  public Course(String name) {
     this.name = name;
-    this.url = url;
   }
 
   public int getId() {
@@ -28,27 +26,18 @@ public class Course {
     this.name = name;
   }
 
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Course course = (Course) o;
     return id == course.id &&
-        name.equals(course.name) &&
-        Objects.equals(url, course.url);
+        name.equals(course.name)
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name, url);
+    return Objects.hash(id, name);
   }
 
   @Override
@@ -56,7 +45,6 @@ public class Course {
     return "Course{" +
         "id=" + id +
         ", name='" + name + '\'' +
-        ", url='" + url + '\'' +
         '}';
   }
 }
---
+++ starter_code/src/main/java/model/Note.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Objects;
+
+public class Note {
+    private int id;
+    private int courseId;
+    private String title;
+    private String creator;
+
+    public Note (int courseId, String title, String creator) {
+        this.courseId = courseId;
+        this.title = title;
+        this.creator = creator;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return id == note.id &&
+                courseId == note.courseId &&
+                title.equals(note.title) &&
+                creator.equals(note.creator);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseId, title, creator);
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", courseId='" + courseId + '\'' +
+                ", title='" + title + '\'' +
+                ", creator='" + creator + '\'' +
+                '}';
+    }
+}
