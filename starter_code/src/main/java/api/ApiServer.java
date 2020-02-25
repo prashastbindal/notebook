67969afcd78906d3513de60fcb8124f3ca2d0488
Felix Parker <flixpar@gmail.com>
2020-02-25 14:51:24 -0500

    added basic course list ui

--- starter_code/src/main/java/api/ApiServer.java
+++ starter_code/src/main/java/api/ApiServer.java
@@ -11,13 +11,13 @@ import model.Course;
 import model.Note;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import io.javalin.plugin.rendering.template.JavalinMustache;
+import io.javalin.plugin.rendering.template.TemplateUtil;
 
 import java.util.List;
 
 public class ApiServer {
   public static void main(String[] args) {
-
-
     // create a database
     Sql2o sql2o = createSql2o();
     // add Courses and Notes tables to database
@@ -29,15 +29,24 @@ public class ApiServer {
     // Run the server
     Javalin app = createJavalinServer();
 
+    Course c1 = new Course("Example Course 1");
+    Course c2 = new Course("Example Course 1");
+    courseDao.add(c1);
+    courseDao.add(c2);
+
     // HTTP Get for the fist page
     app.get("/", ctx -> ctx.result("Welcome to NoteBook APP!"));
 
     // Get the HTTP Get request and respond with courses in the database
-    app.get("/courses", ctx -> {
+    app.get("/courses_json", ctx -> {
       List<Course> courses = courseDao.findAll();
       ctx.json(courses);
       ctx.status(200);
       ctx.contentType("application/json");
+    });
+
+    app.get("/courses", ctx -> {
+      ctx.render("/courses.mustache", TemplateUtil.model("courseList", courseDao.findAll()));
     });
 
     app.post("/courses", ctx -> {
@@ -103,6 +112,9 @@ public class ApiServer {
     JavalinJson.setToJsonMapper(gson::toJson);
     JavalinJson.setFromJsonMapper(gson::fromJson);
     final int PORT = 7000;
+//    return Javalin.create(config -> {
+//      config.addStaticFiles("resources");
+//    }).start(PORT);
     return Javalin.create().start(PORT);
   }
 
