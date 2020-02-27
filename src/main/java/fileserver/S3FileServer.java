af9adf5dc2795a8e8cd2a65c96875f632b7e7afe
Felix Parker <flixpar@gmail.com>
2020-02-26 19:22:06 -0500

    implemented s3 file server

--- src/main/java/fileserver/S3FileServer.java
+++ src/main/java/fileserver/S3FileServer.java
@@ -1,15 +1,57 @@
 package fileserver;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
 
 public class S3FileServer implements FileServer {
 
+    private String bucketName;
+    private AmazonS3 s3Client;
+
+    public S3FileServer() {
+        bucketName = System.getenv("AWS_S3_BUCKET");
+
+        AWSCredentialsProvider awsCredentials = new EnvironmentVariableCredentialsProvider();
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(awsCredentials)
+                .withRegion(Regions.DEFAULT_REGION)
+                .build();
+    }
+
     public void upload(InputStream file, int courseId, int noteId) {
-        throw new UnsupportedOperationException();
+        String filepath = courseId + "/" + noteId + ".pdf";
+        PutObjectRequest uploadRequest = new PutObjectRequest(bucketName, filepath, file, new ObjectMetadata());
+        s3Client.putObject(uploadRequest);
     }
 
     public String getURL(int courseId, int noteId) {
-        throw new UnsupportedOperationException();
+        String objectKey = courseId + "/" + noteId + ".pdf";
+
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60;
+        expiration.setTime(expTimeMillis);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, objectKey)
+                        .withMethod(HttpMethod.GET)
+                        .withExpiration(expiration);
+        URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return url.toString();
     }
 
 }
