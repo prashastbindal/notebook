bef7c40928fc8328a7cc5502daa4186ebc6c5a7b
Felix Parker <flixpar@gmail.com>
2020-04-02 10:52:33 -0400

    debug

--- src/main/java/fileserver/OCRTextExtractor.java
+++ src/main/java/fileserver/OCRTextExtractor.java
@@ -6,14 +6,7 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
 import java.io.File;
-import java.util.Iterator;
-
-import com.github.jaiimageio.plugins.tiff.TIFFImageReadParam;
-import com.github.jaiimageio.impl.common.ImageUtil;
-import com.github.jaiimageio.impl.plugins.tiff.TIFFImageReader;
-import com.github.jaiimageio.impl.plugins.tiff.TIFFImageReaderSpi;
 
 public class OCRTextExtractor {
 
@@ -23,54 +16,14 @@ public class OCRTextExtractor {
         instance = new Tesseract();
         instance.setDatapath("src/main/resources/tessdata_min/");
         instance.setLanguage("eng+equ");
-
-        try {
-            Class.forName("com.github.jaiimageio.plugins.tiff.TIFFImageReadParam");
-        } catch (ClassNotFoundException e) {}
-
         ImageIO.scanForPlugins();
     }
 
     public String extractText(String fn) {
 
-        Javalin.log.info("Readers");
-        ImageIO.scanForPlugins();
-        Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("tiff");
-        try {
-            ImageReader reader = readers.next();
-            Javalin.log.info(reader.toString());
-        } catch (Exception e) {
-            Javalin.log.info("Failed to find tiff reader...");
-            e.printStackTrace();
-        }
-        readers = ImageIO.getImageReadersByFormatName("tif");
-        try {
-            ImageReader reader = readers.next();
-            Javalin.log.info(reader.toString());
-        } catch (Exception e) {
-            Javalin.log.info("Failed to find tif reader...");
-            e.printStackTrace();
-        }
-        readers = ImageIO.getImageReadersByFormatName("jpg");
-        try {
-            ImageReader reader = readers.next();
-            Javalin.log.info(reader.toString());
-        } catch (Exception e) {
-            Javalin.log.info("Failed to find jpg reader...");
-            e.printStackTrace();
-        }
-        try {
-            ImageReader reader = new TIFFImageReader(new TIFFImageReaderSpi());
-            Javalin.log.info(reader.toString());
-        } catch (Exception e) {
-            Javalin.log.info("Failed to create tiff reader...");
-            e.printStackTrace();
-        }
-
         Javalin.log.info("PDF fn: " + fn);
         File pdf = new File(fn);
         try {
-            ImageIO.scanForPlugins();
             String fulltext = instance.doOCR(pdf);
             Javalin.log.info("Completed OCR.");
             Javalin.log.info(fulltext.substring(0, 10) + "...");
