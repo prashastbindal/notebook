76ad0811b261d164f6ae824574b9d4c43d4aee92
Felix Parker <flixpar@gmail.com>
2020-04-23 13:19:46 -0400

    implemented new rich text editor

--- src/main/resources/public/js/upload.js
+++ src/main/resources/public/js/upload.js
@@ -5,22 +5,60 @@ var uploadFiles;
 
 var filetypeSelect = document.getElementById("filetype-field");
 
+$(document).ready(function() {
+
+    var fonts = ['arial', 'roboto', 'inconsolata'];
+    var Font = Quill.import('formats/font');
+    Font.whitelist = fonts;
+    Quill.register(Font, true);
+
+    var quill = new Quill('#texteditor', {
+        theme: 'snow',
+        modules: {
+            'toolbar': [
+                [{ 'font': fonts }, { 'size': [] }],
+                [ 'bold', 'italic', 'underline', 'strike' ],
+                [{ 'color': [] }, { 'background': [] }],
+                [{ 'script': 'super' }, { 'script': 'sub' }],
+                [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block' ],
+                [{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
+                [ {'direction': 'rtl'}, { 'align': [] }],
+                [ 'link', 'video', 'formula' ],
+                [ 'clean' ]
+            ],
+            formula: true,
+            syntax: true,
+        },
+    });
+    loadFonts();
+    $('#texteditor-container').hide();
+});
+
+function loadFonts() {
+    window.WebFontConfig = {
+        google: { families: [ 'Inconsolata::latin', 'Roboto+Slab::latin' ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
+}
+
 filetypeSelect.addEventListener("change", function() {
     if (filetypeSelect.value == "pdf") {
-        $('#texteditor').trumbowyg('destroy');
-        $('#texteditor').hide();
+        $('#texteditor-container').hide();
         $('#file-area').show();
     } else {
-        $('#texteditor').show();
-        $('#texteditor').trumbowyg({
-            resetCss: true,
-            removeformatPasted: true
-        });
+        $('#texteditor-container').show();
         $('#file-area').hide();
     }
 });
 $('#note-form').submit(function() {
-    document.getElementById("textarea").innerHTML = $('#texteditor').trumbowyg('html');
+    document.getElementById("textarea").innerHTML = quill.root.innerHTML;
 });
 
 var addFileName = function(name) {
